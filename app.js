88b354eaf6851581ee6d7e0d0efdee5c0dcde386
jlentz17 <64111084+jlentz17@users.noreply.github.com>
2021-06-21 23:52:11 -0700

    added first question

--- app.js
+++ app.js
@@ -1,19 +1,14 @@
 const inquirer = require("inquirer");
 
-inquirer 
+inquirer
   .prompt([
     {
-
-    },
-
-    {
-
-    },
-
-    {
-
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
     }
   ])
+  .then(answers => console.log(answers));
 // const fs = require("fs");
 
 // const generatePage = require("./src/page-template");
