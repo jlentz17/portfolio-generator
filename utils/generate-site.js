fec6be33cf8d31a418e973c233a447c48f360a64
jlentz17 <64111084+jlentz17@users.noreply.github.com>
2021-06-23 12:41:27 -0700

    refactored

--- utils/generate-site.js
+++ utils/generate-site.js
@@ -1,3 +1,4 @@
+const fs = require("fs");
 const writeFile = (fileContent) => {
   return new Promise((resolve, reject) => {
     fs.writeFile("./dist/index.html", fileContent, (err) => {
@@ -17,9 +18,9 @@ const writeFile = (fileContent) => {
   });
 };
 
-const copyFile = (fileContent) => {
+const copyFile = () => {
   return new Promise((resolve, reject) => {
-    fs.copyFile("./app.js", fileContent, (err) => {
+    fs.copyFile("./src/style.css", "./dist/style.css", (err) => {
       // if there's an error, reject the Promise and send the error to the Promise's `.catch()` method
       if (err) {
         reject(err);
@@ -30,10 +31,11 @@ const copyFile = (fileContent) => {
       // if everything went well, resolve the Promise and send the successful data to the `.then()` method
       resolve({
         ok: true,
-        message: "File copied!",
+        message: "Stylesheet created!",
       });
     });
   });
 };
 
+module.exports = { writeFile, copyFile };
 
